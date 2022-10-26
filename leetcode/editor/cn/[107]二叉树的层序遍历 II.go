3f7192eaf44c5b133b22ce3891d5c5589a0b8835
yeweiting <aA1193504352>
2022-10-26 17:17:56 +0800

    102 & 107

---
+++ leetcode/editor/cn/[107]二叉树的层序遍历 II.go
@@ -0,0 +1,85 @@
+//给你二叉树的根节点 root ，返回其节点值 自底向上的层序遍历 。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历） 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：[[15,7],[9,20],[3]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1]
+//输出：[[1]]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 2000] 内 
+// -1000 <= Node.val <= 1000 
+// 
+//
+// Related Topics 树 广度优先搜索 二叉树 👍 626 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package main
+
+func levelOrderBottom(root *TreeNode) [][]int {
+	res := make([][]int, 0)
+	if root == nil {
+		return res
+	}
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+	for root != nil && len(queue) > 0 {
+		arr := make([]int, 0)
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			if node != nil {
+				arr = append(arr, node.Val)
+			}
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+			queue = queue[1:]
+		}
+		res = append(res, arr)
+	}
+	res  = rev(res)
+	return res
+}
+
+func rev(slice [][]int) [][]int {
+
+	for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
+		slice[i],slice[j] = slice[j],slice[i]
+	}
+	return slice
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
